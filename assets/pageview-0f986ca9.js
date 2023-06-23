9dddb7a039acef768a42b3956fede6f774a94843
asforer <100556512+asforer@users.noreply.github.com>
2023-06-23 01:48:12 +0000

    deploy

---
+++ assets/pageview-0f986ca9.js
@@ -0,0 +1,1 @@
+import{P as e}from"./app-eb7ebb43.js";const a=()=>e({serverURL:""});export{a as updatePageview};
--- assets/pageview-2be9a301.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./app-74115cbf.js";const a=()=>e({serverURL:""});export{a as updatePageview};
