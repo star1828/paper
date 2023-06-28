0334820834cf603535e51cac1ccde9307c43f928
swordsas <121794192+swordsas@users.noreply.github.com>
2023-06-28 09:32:14 +0000

    deploy

--- assets/pageview-0f986ca9.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./app-eb7ebb43.js";const a=()=>e({serverURL:""});export{a as updatePageview};
---
+++ assets/pageview-b1e34587.js
@@ -0,0 +1,1 @@
+import{P as e}from"./app-97951efa.js";const a=()=>e({serverURL:""});export{a as updatePageview};
